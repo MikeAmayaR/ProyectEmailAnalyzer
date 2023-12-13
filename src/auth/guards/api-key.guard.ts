e173993c62f0a5c85100304da7503b0393ffc757
unknown <michaelr.parkit@gmail.com>
2023-12-12 20:13:46 -0500

    adjutments jwt token

--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -5,36 +5,27 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { Observable } from 'rxjs';
-import { ProfileUserClientService } from '../../modules/service/user.client.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  constructor(private readonly jwtService: ProfileUserClientService) {}
+  constructor(private readonly jwtService: JwtService) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<Request>();
-    console.log(
-      '🚀 ~ file: api-key.guard.ts:19 ~ ApiKeyGuard ~ request:',
-      request,
-    );
     const authHeader = request.header('Authorization');
+
     if (!authHeader) {
       throw new UnauthorizedException('Authorization header is missing.');
     }
+
     const [bearerKeyword, token] = authHeader.split(' ');
     if (bearerKeyword !== 'Bearer' || !token) {
       throw new UnauthorizedException('Invalid authorization format.');
     }
+
     try {
-      const decodedToken = this.jwtService.verifyJWT(token);
-      console.log(
-        '🚀 ~ file: api-key.guard.ts:29 ~ ApiKeyGuard ~ decodedToken:',
-        decodedToken,
-      );
-      // request. = decodedToken;
+      this.jwtService.verify(token);
       return true;
     } catch (error) {
       throw new UnauthorizedException('Invalid or expired token');
